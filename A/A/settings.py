ae0c314a560eacf7cf407d1489fb7b0fd169475b
Amir Shabani <129328075+amirshabaniv@users.noreply.github.com>
2023-08-27 03:36:38 +0330

    Update settings.py

--- A/A/settings.py
+++ A/A/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'amirshabaniv@gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'ontfdjstfdvbwapn'
+EMAIL_HOST_PASSWORD = 'key'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_USER = 'Amir website'
